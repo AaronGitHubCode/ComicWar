b8078395b77021a72288981ecaddb23ef2cf9c61
AaronGitHubCode <aarongomezlajarin@gmail.com>
2024-08-10 16:18:25 +0200

    Añadida la carga de imagen y título de cada cómic en el adaptador.

--- app/src/main/java/app/comicwar/comic/ComicsPageAdapter.kt
+++ app/src/main/java/app/comicwar/comic/ComicsPageAdapter.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import android.widget.ImageView
+import android.widget.TextView
+
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.squareup.picasso.Picasso
 
 typealias ComicViewHolder = ComicsPageAdapter.ComicViewHolder
 
@@ -21,6 +25,12 @@ class ComicsPageAdapter(private val comics: List<Comic>) : Adapter<ComicViewHold
     ): ComicViewHolder = ComicViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.comic_item_layout, parent, false))
 
     override fun onBindViewHolder(holder: ComicViewHolder, position: Int) = holder.root.run {
+        val comicPortrait = findViewById<ImageView>(R.id.comic_portrait)
+        Picasso.get().load(comics[position].images[0].path).into(comicPortrait)
+
+        val comicTitle = findViewById<TextView>(R.id.comic_title)
+        comicTitle.text = comics[position].title
+
         setOnClickListener {
             onComicSelect(comics[position])
         }
