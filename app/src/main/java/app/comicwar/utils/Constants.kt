5df66a664434ac51b81e3839abd834becbd9f44f
AaronGitHubCode <aarongomezlajarin@gmail.com>
2024-08-11 02:12:19 +0200

    Eeeiii pillin, la clave privada ya no se ve e.e

--- app/src/main/java/app/comicwar/utils/Constants.kt
+++ app/src/main/java/app/comicwar/utils/Constants.kt
@@ -1,4 +1,5 @@
 package app.comicwar.utils
 
+const val API_URL = "https://comicwar.com"
+
 const val PUBLIC_API_KEY = "05c2c59b15328c627428c4693bcf14b5"
-const val PRIVATE_API_KEY = "372b78c389ecc28ddb03282aaeb1d9d0cbaf479d"
