621e12d40b55b48dacd6a05a64528ed019970547
AaronGitHubCode <aarongomezlajarin@gmail.com>
2024-08-12 12:27:38 +0200

    Añadida URL de la API oficial de Marvel.

--- app/src/main/java/app/comicwar/utils/Constants.kt
+++ app/src/main/java/app/comicwar/utils/Constants.kt
@@ -1,5 +1,5 @@
 package app.comicwar.utils
 
-const val API_URL = "https://comicwar.com"
+const val API_URL = "https://gateway.marvel.com/v1/public/"
 
 const val PUBLIC_API_KEY = "05c2c59b15328c627428c4693bcf14b5"
